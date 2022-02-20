08dcb4519f0f23bb468fb9b9e61c2eeca3431586
Kevin Loughead <kvnloughead@gmail.com>
2022-02-20 10:31:50 -0500

    Final solution of isogram.c

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,22 +1,19 @@
 #include <string.h>
 #include <ctype.h>
-#include <stdio.h>
 #include "isogram.h"
 
 bool is_isogram(const char phrase[]) {
   if (phrase == NULL)
     return false;
-  char ch;
-  for (int i = 0; i < (int) strlen(phrase); i++) {
+  char found[26] = {0};
+  unsigned char ch;
+  for (int i = 0; phrase[i] != '\0'; i++) {
     ch = tolower(phrase[i]);
-    if (ch == '-' || ch == ' ')
-      continue;
-    else {
-      for (int j = i + 1; j < (int) strlen(phrase); j++) {
-        if (ch == tolower(phrase[j]))
-          return false;
-      }
+    if (ch >= 'a' && ch <= 'z') {
+      if (found[ch - 'a'] > 0)
+        return 0;
+      found[ch - 'a'] += 1;
     }
   }
-  return true;
+  return 1;
 }
